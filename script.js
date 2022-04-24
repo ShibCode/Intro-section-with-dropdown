e1611616e8ae68d5b7163fa76b190839bdb189b5
Shoaib Afzaal <100206484+ShibCode@users.noreply.github.com>
2022-04-25 02:05:15 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
-function hammenuClose(){
-    const hammenu = document.getElementById('hamMenu')
-    const features = document.getElementById('featuresExpanded')
-    const company = document.getElementById('companyExpanded')
-    hammenu.style.display = 'none'
-    featuresArr = Array.from(features.classList)
-    if(featuresArr.indexOf('show') != -1){
-        expandFeatures()
-    }
-    companyArr = Array.from(company.classList)
-    if (companyArr.indexOf('show') != -1){
-        expandCompany()
-    }
+function hammenuClose() {
+  const hammenu = document.getElementById("hamMenu");
+  const menu = document.getElementById("Menu");
+  const blackedOut = document.getElementById("blackedOut");
+  const features = document.getElementById("featuresExpanded");
+  const company = document.getElementById("companyExpanded");
+  menu.style.right = "-100%";
+  blackedOut.style.backgroundColor = "hsla(0, 0%, 0%, 0)";
+  featuresArr = Array.from(features.classList);
+  if (featuresArr.indexOf("show") != -1) {
+    expandFeatures();
+  }
+  companyArr = Array.from(company.classList);
+  if (companyArr.indexOf("show") != -1) {
+    expandCompany();
+  }
 }
-function hammenuOpen(){
-    const hammenu = document.getElementById('hamMenu')
-    hammenu.style.display = 'block'
+function hammenuOpen() {
+  const hammenu = document.getElementById("hamMenu");
+  const menu = document.getElementById("Menu");
+  const blackedOut = document.getElementById("blackedOut");
+  blackedOut.style.backgroundColor = "hsla(0, 0%, 0%, 0.2)";
+  menu.style.right = "0";
 }
-function expandFeatures(){
-    const features = document.getElementById('featuresExpanded')
-    const arrowDown = document.getElementById('arrowDown1')
-    const arrowUp = document.getElementById('arrowUp1')
-    features.classList.toggle('show')
-    arrowDown.classList.toggle('show')
-    arrowUp.classList.toggle('hide')
+function expandFeatures() {
+  const features = document.getElementById("featuresExpanded");
+  const featuresOpen = document.getElementById("features");
+  const arrowDown = document.getElementById("arrowDown1");
+  const arrowUp = document.getElementById("arrowUp1");
+  features.classList.toggle("show");
+  featuresOpen.classList.toggle("features-expanded-padding");
+  features.classList.toggle("features-expanded-height");
+  arrowDown.classList.toggle("show-arrow");
+  arrowUp.classList.toggle("hide-arrow");
 }
-function expandCompany(){
-    const company = document.getElementById('companyExpanded')
-    const arrowDown = document.getElementById('arrowDown2')
-    const arrowUp = document.getElementById('arrowUp2')
-    company.classList.toggle('show')
-    arrowDown.classList.toggle('show')
-    arrowUp.classList.toggle('hide')
+function expandCompany() {
+  const company = document.getElementById("companyExpanded");
+  const arrowDown = document.getElementById("arrowDown2");
+  const arrowUp = document.getElementById("arrowUp2");
+  const companyOpen = document.getElementById("company");
+  company.classList.toggle("show");
+  company.classList.toggle("companu-expanded-height");
+  companyOpen.classList.toggle("company-expanded-padding");
+  arrowDown.classList.toggle("show-arrow");
+  arrowUp.classList.toggle("hide-arrow");
 }
